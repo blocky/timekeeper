796233256372384255be03d1a624d91a127455da
ian hecker <ian@blocky.rocks>
2023-06-12 13:31:32 -0600

    Autoset year from every prompt. Update parsing

--- timekeep/timekeep.go
+++ timekeep/timekeep.go
@@ -9,30 +9,73 @@ import (
 	"strings"
 )
 
+type Month int
+
+func MakeMonth(m int) (Month, error) {
+	if 0 >= m || m > 12 {
+		return 0, fmt.Errorf("month is invalid: '%d'", m)
+	}
+	return Month(m), nil
+}
+
+func (m Month) String() string {
+	if m < 10 {
+		return fmt.Sprintf("0%d", m)
+	}
+	return fmt.Sprintf("%d", m)
+}
+
+type Day int
+
+func MakeDay(d int) (Day, error) {
+	if 0 >= d || d > 31 {
+		return 0, fmt.Errorf("day is invalid: '%d'", d)
+	}
+	return Day(d), nil
+}
+
+func (d Day) String() string {
+	if d < 10 {
+		return fmt.Sprintf("0%d", d)
+	}
+	return fmt.Sprintf("%d", d)
+}
+
+type MilitaryTime string
+
+func MakeMilitaryTime(s string) (MilitaryTime, error) {
+	if len(s) != 4 {
+		return "", fmt.Errorf("provide 4 digits (HourMinutes)")
+	}
+	return MilitaryTime(s), nil
+}
+
 type Task struct {
 	Name    string
 	ID      string
 	project string
 }
 
-type Time struct {
-	Date  string
-	Start string
-	Stop  string
+type Date struct {
+	Year  int
+	Month Month
+	Day   Day
+	Start MilitaryTime
+	Stop  MilitaryTime
 }
 
-func (t Time) String() string {
-	return fmt.Sprintf("%s:%s-%s", t.Date, t.Start, t.Stop)
+func (t Date) String() string {
+	return fmt.Sprintf("%d-%s-%s:%s-%s", t.Year, t.Month, t.Day, t.Start, t.Stop)
 }
 
 type Entry struct {
-	Time    Time
+	Date    Date
 	Task    Task
 	Details string
 }
 
 func (e Entry) String() string {
-	return fmt.Sprintf("## %s %s %s %s\n", e.Time, e.Task.project, e.Task.ID, e.Details)
+	return fmt.Sprintf("## %s %s %s %s\n", e.Date, e.Task.project, e.Task.ID, e.Details)
 }
 
 var tasks = map[int]Task{
@@ -59,7 +102,7 @@ func printTasks() {
 	var list = make([]int, len(tasks))
 	var i int
 
-	for key, _ := range tasks {
+	for key := range tasks {
 		list[i] = key
 		i++
 	}
@@ -80,25 +123,50 @@ func MakeTimekeeper(f *os.File) Timekeeper {
 	return Timekeeper{w}
 }
 
-func (tk *Timekeeper) AskTime() (Time, error) {
-	day := tk.Ask("what day? (YYYY-MM-DD)")
-	start := tk.Ask("start hour? (HHHH)")
-	stop := tk.Ask("stop hour? (HHHH)")
-	return Time{Date: day, Start: start, Stop: stop}, nil
+func (tk *Timekeeper) AskTime() (Date, error) {
+	year := 2023
+
+	month, err := tk.AskMonth("what month? (integer)")
+	if err != nil {
+		return Date{}, err
+	}
+
+	day, err := tk.AskDay("what day? (integer)")
+	if err != nil {
+		return Date{}, err
+	}
+
+	start, err := tk.AskMilitaryTime("start hour? (HourMinutes)")
+	if err != nil {
+		return Date{}, err
+	}
+
+	stop, err := tk.AskMilitaryTime("stop hour? (HourMinutes)")
+	if err != nil {
+		return Date{}, err
+	}
+
+	date := Date{
+		Year:  year,
+		Month: month,
+		Day:   day,
+		Start: start,
+		Stop:  stop,
+	}
+	return date, nil
 }
 
 func (tk *Timekeeper) AskTask() (Task, error) {
 	printTasks()
 
-	num := tk.Ask("what task? (id number)")
-	taskNum, err := strconv.Atoi(num)
+	num, err := tk.AskInt("what task? (id number)")
 	if err != nil {
-		return Task{}, fmt.Errorf("Given non int string: %s", num)
+		return Task{}, err
 	}
 
-	task, exists := tasks[taskNum]
+	task, exists := tasks[num]
 	if !exists {
-		return Task{}, fmt.Errorf("No task exists for number:%d", taskNum)
+		return Task{}, fmt.Errorf("No task exists for number:%d", num)
 	}
 	return task, nil
 }
@@ -121,7 +189,7 @@ func (tk *Timekeeper) MakeEntry() (Entry, error) {
 	details := tk.AskDetails()
 
 	return Entry{
-		Time:    time,
+		Date:    time,
 		Task:    task,
 		Details: details,
 	}, nil
@@ -139,6 +207,47 @@ func (tk *Timekeeper) WriteEntry(entry Entry) error {
 func (tk *Timekeeper) Ask(question string) string {
 	answer := prompt(question)
 	return answer
+}
+
+func (tk *Timekeeper) AskInt(question string) (int, error) {
+	answer := tk.Ask(question)
+	i, err := strconv.Atoi(answer)
+	if err != nil {
+		return 0, fmt.Errorf("given non-int: '%s'", answer)
+	}
+	return i, nil
+}
+
+func (tk *Timekeeper) AskMonth(question string) (Month, error) {
+	m, err := tk.AskInt(question)
+	if err != nil {
+		return 0, err
+	}
+	month, err := MakeMonth(m)
+	if err != nil {
+		return 0, err
+	}
+	return month, nil
+}
+
+func (tk *Timekeeper) AskDay(question string) (Day, error) {
+	d, err := tk.AskInt(question)
+	if err != nil {
+		return 0, err
+	}
+	day, err := MakeDay(d)
+	if err != nil {
+		return 0, err
+	}
+	return day, nil
+}
+
+func (tk *Timekeeper) AskMilitaryTime(question string) (MilitaryTime, error) {
+	mt, err := MakeMilitaryTime(tk.Ask(question))
+	if err != nil {
+		return "", err
+	}
+	return mt, nil
 }
 
 func prompt(label string) string {

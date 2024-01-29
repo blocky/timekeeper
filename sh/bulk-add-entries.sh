4ff51abea229a346937989eccc1f9e5e29c0505f
ian hecker <ian@blocky.rocks>
2024-01-29 12:46:54 -0700

    Feature[bulk add entries] Add script

---
+++ sh/bulk-add-entries.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+# 651ab5fe700d5d4652866ca0:'Ca$h In'
+# 5f91eca7b1d41c38c2d67837:'Dugnad'
+# 659f5c0e32b33d164101f791:'EVM(+) Design'
+# 61f15163c32016098b513e86:'Grant Admin'
+# 61a4d4653b4281137e936f7c:'Holiday'
+# 5f91ef2eb1d41c38c2d6932f:'Infinity Pool of Crises'
+# 6304cf552f286f53f966fbcc:'Market Research'
+# 6109a1f50a335f28d1599ef3:'Meetings'
+# 643406065cbbcc37df577158:'NSDI Submission'
+# 61a4d46a86b8855e51fce0d9:'Sick Leave'
+# 61a4d4733b4281137e937078:'Vacation'
+# 643405bbcb8fab2f0d10f430:'White Paper 0.3'
+# 651ab6a9d75fb56358cd1269:'ZPR - Monitor'
+# 651ab6930e64b358d5f753c5:'ZPR - Performance Metrics: Running and Reporting'
+# 656aa57b3ce2e9386eb114e0:'ZPR - Public API'
+# 63aee21ca3e51625ef081f39:'ZPR - Strong Verify'
+# 64a6e9f289058175da4aa74d:'ZPR - Verify Config'
+
+tk add entry -m= -d= --start= --stop= --task-id= --details=""
